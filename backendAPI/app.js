7327c79662e642d537eb74126d70d8e77416b6b8
Leonardo Muzi <leomuzicarvalho@gmail.com>
2020-01-22 13:13:57 -0500

    connection to db in progress...

--- backendAPI/app.js
+++ backendAPI/app.js
@@ -1,7 +1,13 @@
 const express = require('express')
+require('dotenv').config()
 const app = express()
+const MongoClient = require('mongodb').MongoClient;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0-xj677.mongodb.net/test?retryWrites=true&w=majority`;
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => {
+    res.send(`Hello World! ${process.env.DB_HOST}`)
+})
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`TODO listening on port ${port}!`))
+
