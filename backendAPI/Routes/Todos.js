63153ad57e4259d63079b7547effaae08e8abc2b
Leonardo Muzi <leomuzicarvalho@gmail.com>
2020-01-29 21:32:53 -0500

    added more methods to TODO router

--- backendAPI/Routes/Todos.js
+++ backendAPI/Routes/Todos.js
@@ -1,18 +1,59 @@
 const express = require("express");
-const todo = require('../models/Todo');
+const Todo = require('../models/Todo');
+const mongo = require('mongodb')
 
 const router = express.Router();
 
-router.get('/', (req,res) => {
-    res.send("We are on TODOS!");
+
+router.get('/all', async (req,res) => {
+    try{
+        const todo = await Todo.find()
+        res.json(todo)
+    }
+    catch(err){
+        res.json({message: err})
+    }
+    
 });
 
-router.post('/createTODOtest', (req, res) => {
-    var newTodo = new todo({ title: req.body.title, description: req.body.description, deadline: new Date() });
+router.get('/find/:id', async (req,res) => {
+    try{
+        const todo = await Todo.find({"_id" : new mongo.ObjectID(req.params.id)})
+        res.json(todo)
+    }
+    catch(err){
+        res.json({message: err})
+    }
+    
+});
+
+router.post('/createTODO', (req, res) => {
+    var newTodo = new Todo({ title: req.body.title, description: req.body.description, deadline: new Date() });
 
     newTodo.save()
     .then(data => {
         res.json(data);
+    })
+    .catch(err => {
+        res.json({message: err});
+    })
+})
+
+router.put('/editTODO/:id', (req, res) => {
+    Todo.updateOne({"_id" : new mongo.ObjectID(req.body.id)}, {$set: {title : req.body.newTitle, description: req.body.newDescription}})
+    .then(data => {
+        res.json({message: "Todo successfully updated!"});
+    })
+    .catch(err => {
+        res.json({message: err});
+    })
+})
+
+router.post('/deleteTODO', (req, res) => {
+    
+    Todo.deleteOne({"_id" : new mongo.ObjectID(req.body.id)})
+    .then(data => {
+        res.json({message: "Todo deleted!"});
     })
     .catch(err => {
         res.json({message: err});
